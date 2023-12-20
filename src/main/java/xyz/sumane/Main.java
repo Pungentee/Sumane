f6b5100a9b0aa0bc9dcd5e99582f0544a01935b3
Tymofii Kliuiev <pungentee@gmail.com>
2023-12-20 16:27:17 +0200

    initialization commit

---
+++ src/main/java/xyz/sumane/Main.java
@@ -0,0 +1,22 @@
+package xyz.sumane;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+import java.util.Properties;
+
+public class Main {
+    public static void main(String[] args) {
+        final String TOKEN = System.getenv("TOKEN");
+
+        try {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            botsApi.registerBot(new Bot(TOKEN));
+
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
